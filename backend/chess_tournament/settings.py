958641862667020cf8333593fd81a0c30309399f
Geronimo Errante <geronimoerrante7@gmail.com>
2025-05-17 00:56:59 -0300

    back: config django con postgres

--- backend/chess_tournament/settings.py
+++ backend/chess_tournament/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'chess_tournament.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'chess_tournament',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
